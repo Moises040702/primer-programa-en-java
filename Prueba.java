b87bcdf2e9fafbe9474c8d296b4861960e19ab06
Moises <moises.celmar@educa.jcyl.es>
2022-09-28 11:11:17 +0200

    cantante favorito

--- Prueba.java
+++ Prueba.java
@@ -2,9 +2,9 @@ public class Prueba {
 
     public static void main(String args[]) {
 
-        System.out.println("Cela Martínez");
 	  System.out.println("20 años, 2002");
 	  System.out.println("Pasta");
+	  System.out.println("Bad Bunny");
     }
 
 }
